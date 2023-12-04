919615aae60263b6fb73222969a22647eaac7eb3
maxCentaur <ambition_x@163.com>
2023-12-04 10:36:01 +0800

    feat: unified APG Version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
-    id("com.android.library") version "8.1.0" apply false
+    id("com.android.application") version Version.AGP apply false
+    id("com.android.library") version Version.AGP apply false
+    id("org.jetbrains.kotlin.android") version Version.kotlin apply false
 }
 buildscript {
     dependencies {
